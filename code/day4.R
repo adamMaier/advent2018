41ed809d9870334779c8d810bdd047ad3a135263
Adam <adam.maier@tntp.org>
2018-12-04 18:44:17 -0500

    Day 4 edits and complete part 2

--- code/day4.R
+++ code/day4.R
@@ -11,6 +11,37 @@ data <- read_delim(
 )
 
 
+# WRITE FUNCTIONS ----------------------------------------------------------------------------------
+
+# Write a function to create a vector given start and end sleep times
+create_vec <- function(start = NULL, end = NULL) {
+  if (start != 0) {
+    c(rep(0, start - 1), rep(1, end - start), rep(0, 1 + 60 - end))
+  } else {
+    c(rep(1, end - 1), rep(0, 1 + 60 - end))
+  }
+}
+
+# Write a function to extract start and end times from a chronological order of sellp then wake then
+# slepp then... wake
+time_extract <- function(data = NULL) {
+  
+  starts_sleep <- data %>%
+    filter(action == "falls asleep") %>%
+    pull(minute)
+  
+  ends_sleep <- data %>%
+    filter(action == "wakes up") %>%
+    pull(minute)
+  
+  out_vec <- rep(0, 60)
+  for (i in 1:length(starts_sleep)) {
+    new_vec <- create_vec(start = starts_sleep[i], end = ends_sleep[i])
+    out_vec <- out_vec + new_vec
+  }
+  out_vec
+}
+
 
 # PART 1 -------------------------------------------------------------------------------------------
 
@@ -48,23 +79,35 @@ p1_id <- p1_data %>%
 
 # Find the minute most slept by keeping records of id and converting them to vectors of 60, then
 # summing
-starts_sleep <- p1_data %>%
-  filter(id == p1_id & action == "falls asleep") %>%
-  pull(minute)
-
-ends_sleep <- p1_data %>%
-  filter(id == p1_id & action == "wakes up") %>%
-  pull(minute)
-
-result <- rep(0, 60)
-for (i in 1:length(starts_sleep)) {
-  new_vec <- c(
-    rep(0, starts_sleep[i] - 1), 
-    rep(1, ends_sleep[i] - starts_sleep[i]),
-    rep(0, 1 + 60 - ends_sleep[i])
-  )
-  result <- result + new_vec
-}
+result <- time_extract(p1_data %>% filter(id == p1_id))
 p1_minute <- which(result == max(result))
 
+# Print result
 p1_minute * as.numeric(p1_id)
+
+
+
+# PART 2 -------------------------------------------------------------------------------------------
+
+# Using prepped p1 data, group by ID and apply function to all IDs
+p2_data <- split(p1_data, f = p1_data$id)
+
+# For each ID, identify the max number of overlapped minutes, then the max of the max
+p2_data <- 
+  lapply(p2_data, function(x) {
+    vec <- time_extract(x)
+    out_minute <- which(vec == max(vec))
+    out_total <- max(vec)
+    c(out_total, out_minute)
+    }
+  )
+
+# Identify individual with most frequent minute slept
+p2_id <-
+  lapply(p2_data, function(x) x[1]) %>%
+  unlist()
+
+p2_id <- names(p2_id[which(p2_id == max(p2_id))])
+
+# Multiplying result
+as.numeric(p2_id) * p2_data[[p2_id]][2]
